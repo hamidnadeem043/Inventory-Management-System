35910eea7cce75f3f8ace367724277befa78f6f9
Hamid Nadeem <hamidnadeemn043@gmail.com>
2026-06-15 13:47:42 +0500

    Fix database connection for Railway

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ app.use(session({
 }));
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "inventory_app",
-    password: "iamalpha)(1)"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT
 });
 
 const isLoggedIn = (req, res, next) => {
